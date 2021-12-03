0c636c95e5a06516f10f77eff537c004fe9a9b9d
T.Michel <theo.michel2@outlook.fr>
2021-12-03 09:23:44 +0100

    listen checkbox and apply style to checked task

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -7,11 +7,12 @@ class task{  //On crée l'objet task
 
 let form = document.getElementsByTagName("form")[0];//On récupere le formulaire
 let tabTask = [];//stockage de toute les tasks
+let taskDesc; //stockage pour la description de la tache
 
 //On ecoute le click sur l'envoi du champ
 document.getElementById("sendTask").addEventListener("click", function(event){
     event.preventDefault();//on empeche le chargement de la page
-    let taskDesc = document.getElementById("newTask").value;//on stock le contenu du champ
+    taskDesc = document.getElementById("newTask").value;//on stock le contenu du champ
     document.getElementById("newTask").value = "";//On efface le champ
     tabTask.push(new task(taskDesc));//On ajoute la nouvelle tache dans le tableau de stockage
     displayTask(tabTask[tabTask.length-1]);//On affiche le dernier élément crée avec la fonction displayTask
@@ -21,6 +22,22 @@ document.getElementById("sendTask").addEventListener("click", function(event){
 function displayTask(task){
     console.log(task.description);
     let newTask = document.createElement("div"); //On crée une div qui stockera la tache
+    newTask.id = "task-" + tabTask.indexOf(task);//On lie la div à la place de la tache dans notre tableau
     form.appendChild(newTask); //On dispose ensuite ce nouvel élément en enfant du formulaire
-    newTask.innerHTML = '<input type="checkbox" name="" id=""><p>'+ task.description + '</p>'; //Puis on ajoute une checkbox et la description de la tache dans la div
+    newTask.innerHTML = '<input type="checkbox" name="checkbox-'+ tabTask.indexOf(task) +'" onchange="checkTaskStatut(this)"><p>'+ task.description + '</p>'; //Puis on ajoute une checkbox et la description de la tache dans la div
+}
+
+
+//Function executé au changement sur la checkbox
+
+function checkTaskStatut(checkbox){
+    if(checkbox.checked){
+        console.log("coché !");
+        console.log(checkbox.parentElement.id);//recupération d'id du parent
+        document.getElementById(checkbox.parentElement.id).classList.add("done");
+    }
+    else{
+        console.log("pas coché !");
+        document.getElementById(checkbox.parentElement.id).classList.remove("done");
+    }
 }
